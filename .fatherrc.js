544bfcb57a92fed1bbd5d9e70d7425553c9e5116
Jiehai Gu <jiehaigu@JiehaideMacBook-Pro.local>
2020-04-15 13:39:22 +0800

    增加混淆版本

---
+++ .fatherrc.js
@@ -0,0 +1,11 @@
+export default {
+  entry: "lib/kHttpClient.js",
+  esm: {
+    type: "rollup",
+    minify: true,
+  },
+  // doc: {
+  //     themeConfig: { mode: 'dark' },
+  //     base: '/your-repo'
+  // },
+};
